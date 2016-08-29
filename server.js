7581bb6d5a563374f2ba2a77fe336fe11f21eb9a
formatted <ronivegh@gmail.com>
2016-08-29 15:54:06 -0600

    fighting for IP

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.get('/', function (req, res) {
 
 io.on('connection', function (socket){
   // // need to play around w/ this might have the IP :)
-  // var sHeaders = socket.handshake.headers;
-  // console.info(sHeaders);
+  var sHeaders = socket.handshake;
+  console.info(sHeaders);
   // could try this too:
-  // var fooBoo = socket.request.connection.remoteAddress;
-  var fooBoo = socket.request.connection._peername.address;
+  var fooBoo = socket.request.connection.remoteAddress;
+  // var fooBoo = socket.request.connection._peername.address;
   console.log('a user connected', fooBoo);
   socket.on('disconnect', function(){
     console.log('user disconnected');
@@ -28,3 +28,4 @@ io.on('connection', function (socket){
 server.listen(port, function () {
   console.log('Example app listening on port ' + port + '!');
 });
+
